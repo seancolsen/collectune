79f1a619c1ce8b49a09855ddc09be8886a25c00e
Sean Colsen <colsen.sean@gmail.com>
2026-02-21 15:07:24 -0500

    Modify project structure for frontend egui app

---
+++ frontend/src/main.rs
@@ -0,0 +1,21 @@
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Collectune",
+        options,
+        Box::new(|_cc| Ok(Box::new(App::default()))),
+    )
+}
+
+#[derive(Default)]
+struct App;
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Collectune");
+        });
+    }
+}
