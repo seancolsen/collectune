5f2f67b16c8aa7bfc86ccc8809dc6f91b45743d9
Sean Colsen <colsen.sean@gmail.com>
2026-08-20 01:18:40 +0000

    Adjust default ratings

--- backend/src/migrations/0003.sql
+++ backend/src/migrations/0003.sql
@@ -59,9 +59,9 @@ create table rating (
 
 insert into rating (id, value, symbol, description) values
 ('ed9a010a-124b-4aee-bce4-4889875142e8', 1, '🗑️', 'Skip'),
-('ddd714b8-2d6a-4ff0-b280-74d8472116a7', 2, '✔️', 'Like'),
-('7f592dd0-be55-4ef8-a946-1a11cd0d03b5', 3, '❤️', 'Love'),
-('3e056915-37fc-4660-8db5-06c15572591a', 4, '🏆', 'Best');
+('ddd714b8-2d6a-4ff0-b280-74d8472116a7', 2, '👍', 'Like'),
+('7f592dd0-be55-4ef8-a946-1a11cd0d03b5', 3, '⭐', 'Prefer'),
+('3e056915-37fc-4660-8db5-06c15572591a', 4, '❤️', 'Love');
 
 create table migration_0003_track_rating as
 select t.id as track, r.id as rating
